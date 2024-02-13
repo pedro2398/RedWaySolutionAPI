f19a782988089113519049e7ef521b01824bc0ae
pedro2398 <pedrooo2398@gmail.com>
2024-02-13 14:50:09 -0300

    adding user service

---
+++ src/main/java/com/RedWaySolutionAPI/service/UserService.java
@@ -0,0 +1,28 @@
+package com.RedWaySolutionAPI.service;
+
+import com.RedWaySolutionAPI.domain.User;
+import com.RedWaySolutionAPI.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository repositry;
+    public User postUser(User user) {
+        repositry.save(user);
+        return user;
+    }
+
+    public List<User> getUser() {
+        return repositry.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return repositry.findById(id).orElseThrow(() ->
+        new RuntimeException("Unable to perform the request")
+        );
+    }
+}
