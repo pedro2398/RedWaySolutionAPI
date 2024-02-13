71faa82db27d825202f597714ff1d9fd6e8c1c3a
pedro2398 <pedrooo2398@gmail.com>
2024-02-13 14:38:15 -0300

    adding user class

---
+++ src/main/java/com/RedWaySolutionAPI/domain/User.java
@@ -0,0 +1,22 @@
+package com.RedWaySolutionAPI.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TB_USER")
+@Entity(name = "TB_USER")
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+}
