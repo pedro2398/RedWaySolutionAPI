1a2e0668c08fea98655ee0a797897adefb726b28
pedro2398 <pedrooo2398@gmail.com>
2024-02-13 14:21:11 -0300

    Initial Commit

---
+++ src/main/java/com/RedWaySolutionAPI/RedWaySolutionApiApplication.java
@@ -0,0 +1,13 @@
+package com.RedWaySolutionAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedWaySolutionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedWaySolutionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RedWaySolutionAPI/RedWaySolutionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RedWaySolutionAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedWaySolutionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
